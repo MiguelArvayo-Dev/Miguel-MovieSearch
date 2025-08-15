7c7dec78790c158864a69f1f91dc1cd0984b10ef
migue salsa <miguelarvayoPA@gmail.com>
2025-08-14 17:09:17 -0700

    correcting api to https

--- src/services/fecthMovies.js
+++ src/services/fecthMovies.js
@@ -4,7 +4,7 @@ export const searchMovies = async ({ search }) => {
   //if (search === "") return null;
   try {
     const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
     )
     const json = await response.json()
 
